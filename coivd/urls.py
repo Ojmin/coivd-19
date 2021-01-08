16f4a6bd4979f33e458d353a05949d00bb923e33
Ojmin <1543461999@qq.com>
2021-01-08 17:24:09 +0800

    com

--- coivd/urls.py
+++ coivd/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from coivd.app.info.views import CoivdInfo
 
 urlpatterns = [
-    path(r'coivd_info', CoivdInfo.as_view()),
+    path('coivd_info', CoivdInfo.as_view()),
 ]
