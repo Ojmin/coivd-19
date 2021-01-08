33b1f2d643c011d0e896e6e8c03322c5f6b9cd28
Ojmin <1543461999@qq.com>
2021-01-08 13:42:40 +0800

    qq

--- data.py
+++ data.py
@@ -2,3 +2,5 @@
 # https://api.inews.qq.com/newsqa/v1/automation/modules/list?modules=FAutoCountryMerge
 # 国内
 # https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&callback=&_=%d'%int(time.time()*1000)
+#
+# https://view.inews.qq.com/g2/getOnsInfo?name=disease_foreign
